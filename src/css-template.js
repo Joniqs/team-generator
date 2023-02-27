afb698835bfda240727911e17b9e3188b69df9af
Jonatan Kwiatkowski <joniqs91@gmail.com>
2023-02-27 15:09:16 +0000

    Added file creation functions and templates for html and css files.

---
+++ src/css-template.js
@@ -0,0 +1,18 @@
+const renderCss = () => {
+    return `
+    .team-heading {
+        background-color: orangered;
+        color: white;
+      }
+      
+      .card-header {
+        background-color: blueviolet;
+      }
+      
+      .employee-card {
+        margin-left: 4rem;
+      }
+    `;
+  };
+  
+module.exports = renderCss;
